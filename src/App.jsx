ad8dc42863c47cea68931f452ffc5850955d58cf
Min Khant Kyaw <mkhantk102@gmail.com>
2024-07-07 15:53:48 +0630

    Add custom font

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,49 @@ function App() {
 
   return (
     <>
-      <div className='container flex flex-col justify-center items-center min-h-screen'>
-        <header>
-          <h1>Reliable, efficient delivery</h1>
-          <h2>Powered by Technology</h2>
-          <p>Our Artificial Intelligence powered tools use millions of project data points 
+      <div className='container flex flex-col justify-center items-center min-h-screen min-w-full p-5 font-body gap-2'>
+        <header className='flex flex-col justify-center items-center p-2 max-w-[550px]'>
+          <h1 className='text-2xl md:text-3xl font-thin text-veryDarkBlue'>Reliable, efficient delivery</h1>
+          <h2 className='text-2xl md:text-3xl font-semibold text-veryDarkBlue'>Powered by Technology</h2>
+          <p className='text-md text-center my-7'>Our Artificial Intelligence powered tools use millions of project data points 
           to ensure that your project is successful</p>        
         </header>
         
         
-        <main>
-          <div>
-            <h3>Supervisor</h3>
-            <p>Monitors activity to identify project roadblocks</p>
+        <main className='flex flex-col gap-5 m-5 md:flex-row md:items-center'>
+          <div className='cards border-t-cyan-500'>
+            <div>
+              <h3>Supervisor</h3>
+              <p>Monitors activity to identify project roadblocks</p>
+            </div>
+            
             <img src="./public/assets/icon-supervisor.svg" alt="supervisor-icon" />
           </div>
-          <div>
-            <h3>Team Builder</h3>
-            <p>Scans our talent network to create the optimal team for your project</p>
-            <img src="./public/assets/icon-team-builder.svg" alt="team-builder" />
+          <div className='flex flex-col gap-5'>
+            <div className='cards border-t-red-500'>
+              <div>
+                <h3>Team Builder</h3>
+                <p>Scans our talent network to create the optimal team for your project</p>
+              </div>
+              
+              <img src="./public/assets/icon-team-builder.svg" alt="team-builder" />
+            </div>
+            <div className='cards border-t-orange-500'>
+              <div>
+                <h3>Karma</h3>
+                <p>Regularly evaluates our talent to ensure quality</p>
+              </div>
+              
+              <img src="./public/assets/icon-karma.svg" alt="karma-icon" />
+            </div>
           </div>
-          <div>
-            <h3>Karma</h3>
-            <p>Regularly evaluates our talent to ensure quality</p>
-            <img src="./public/assets/icon-karma.svg" alt="karma-icon" />
-          </div>
-          <div>
-            <h3>Calculator</h3>
-            <p> Uses data from past projects to provide better delivery estimates</p>
+          
+          <div className='cards border-t-blue-500'>
+            <div>
+              <h3>Calculator</h3>
+              <p> Uses data from past projects to provide better delivery estimates</p>
+            </div>
+            
             <img src="./public/assets/icon-calculator.svg" alt="calculator-icon" />
           </div>   
               
@@ -45,9 +60,9 @@ function App() {
 
         
         <footer>
-          <p class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+          <p className="attribution text-sm ">
+            Challenge by <a className='text-blue-400' href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+            Coded by <a className='text-blue-400' href="#">Min Khant Kyaw</a>.
           </p>
         </footer>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'veryDarkBlue': 'hsl(234, 12%, 34%)' ,
+        'grayishBlue': 'hsl(229, 6%, 66%)',
+        'veryLightGray': 'hsl(0, 0%, 98%)'
+      },
+      fontFamily:{
+        body: ['Poppins'],
+      }
+    },
   },
   plugins: [],
 }
